2ecca904586ef46cbd62dc6de7f03293d4a27a0e
Likhitha101 <21eg106b52@anurag.edu.in>
2023-10-02 16:35:07 +0530

    my first commit

---
+++ myapp/routes/dist/index.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var express = require('express');
+
+var router = express.Router();
+/* GET home page. */
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'wpm'
+  });
+});
+router.get('/angular', function (req, res, next) {
+  res.render('index', {
+    title: 'wpm -mean'
+  });
+});
+module.exports = router;
